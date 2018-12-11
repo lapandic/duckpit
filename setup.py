d3076d3ae8b22d86489a90037a8bc76430882fdd
lapandic <ldzenan@student.ethz.ch>
2018-12-11 21:38:23 +0100

    gui set, linking in process

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rqt_duckpit'],
     package_dir={'': 'src'},
+    scripts=['scripts/rqt_duckpit']
 )
 
 setup(**d)
